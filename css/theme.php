990cf3c499b78b656be3d1b5d28f309eb9685630
LupusXLass1404 <berkasxlupusxlass@gmail.com>
2024-11-22 10:00:13 +0800

    調整圖片，增加顏色

--- css/theme.php
+++ css/theme.php
@@ -2,32 +2,80 @@
     $c= $month-1;
 
     $color = [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
         [
-            'text' => "rgb(26, 79, 108)",
-            'link'=> "rgb(26, 79, 108)",
-            'button'=> "rgba(161, 205, 255, 0.5)",
-            'special' => "rgb(86, 117, 201)",
+            'title'=> "rgb(74 145 133)", // 灰
+            'button'=> "rgba(127 225 209 / 50%)", 
+            'special' => "rgb(109 231 210)",
+        ],
+        [
+            'title'=> "rgb(119 82 153)", // 紫
+            'button'=> "rgba(188 117 205 / 50%)",
+            'special' => "rgb(231 171 255)",
+        ],
+        [
+            'title'=> "rgb(255 173 199)", // 粉
+            'button'=> "rgba(255 132 190 / 50%)", 
+            'special' => "rgb(255 173 199)",
+        ],
+        [
+            'text' => "rgb(144, 238, 144)", // 淺綠
+            'title'=> "rgb(144, 238, 144)", 
+            'button'=> "rgba(152, 251, 152, 0.5)", 
+            'special' => "rgb(60, 179, 113)",
+        ],
+        [
+            'text' => "rgb(173, 216, 230)", // 淺藍
+            'title'=> "rgb(173, 216, 230)", 
+            'button'=> "rgba(240, 248, 255, 0.5)", 
+            'special' => "rgb(135, 206, 250)",
+        ],
+        [
+            'text' => "rgb(34, 139, 34)", // 珊瑚
+            'title'=> "rgb(34, 139, 34)", 
+            'button'=> "rgba(50, 205, 50, 0.5)", 
+            'special' => "rgb(0, 100, 0)",
+        ],
+        [
+            'text' => "rgb(230, 230, 250)", // 淺紫
+            'title'=> "rgb(230, 230, 250)", 
+            'button'=> "rgba(216, 191, 216, 0.5)", 
+            'special' => "rgb(147, 112, 219)",
+        ],
+        [
+            'text' => "rgb(255, 215, 0)", // 黃
+            'title'=> "rgb(255, 215, 0)", 
+            'button'=> "rgba(255, 255, 224, 0.5)", 
+            'special' => "rgb(240, 230, 140)",
+        ],
+        [
+            'text' => "rgb(139, 0, 0)", // 深紅
+            'title'=> "rgb(139, 0, 0)", 
+            'button'=> "rgba(205, 92, 92, 0.5)", 
+            'special' => "rgb(178, 34, 34)",
+        ],
+        [
+            'text' => "rgb(101, 67, 33)", // 深棕
+            'title'=> "rgb(101, 67, 33)", 
+            'button'=> "rgba(139, 69, 19, 0.5)", 
+            'special' => "rgb(160, 82, 45)",
+        ],
+        [
+            'title'=> "rgb(255 185 80)", // 橘
+            'button'=> "rgba(247 151 65 / 50%)", 
+            'special' => "rgb(255 185 80)",
+        ],
+        [
+            'title'=> "rgb(54 112 173)", // 深藍
+            'button'=> "rgba(135 191 255 / 50%)",
+            'special' => "rgb(141 196 255)",
         ]
     ];
 
 ?>
 
 <style>
-    .month,
-    .month a,
-    .month .bottomButton a {
-        color: <?=$color[$c]['text'];?>;
+    .timeBox p {
+        color: <?=$color[$c]['special'];?>;
     }
 
     .month #dateTable td:nth-child(1) a,
@@ -43,11 +91,11 @@
 
     .month caption,
     .month .tableLink:active {
-        background-color: <?=$color[$c]['link'];?>;
+        background-color: <?=$color[$c]['title'];?>;
     }
 
     .month .tableLink:hover {
-        border: 3px solid <?=$color[$c]['link'];?>;
+        border: 3px solid <?=$color[$c]['special'];?>;
     }
 
     .month .button {
